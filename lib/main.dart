1897c59fd61e7ad068e4bc9c9fcbbc88afe8cda1
Abdullah Johar <jrrabdullah23@gmail.com>
2025-07-25 21:41:17 +0600

    changes due to adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_task/firebase_options.dart';
 import 'package:todo_task/signup_page.dart';
 
-void main() {
+Future<void> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform
+    );
     runApp(const MyApp());
 }
 

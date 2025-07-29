ceaba82e2a86e7f1971cebce19f7e8326c7ab7a2
Abdullah Johar <jrrabdullah23@gmail.com>
2025-07-29 11:56:53 +0600

    just fixing some indentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_task/firebase_options.dart';
 import 'package:todo_task/home_page.dart';
 import 'package:todo_task/signup_page.dart';
+import 'package:todo_task/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
     WidgetsFlutterBinding.ensureInitialized();
